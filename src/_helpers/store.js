7e9992ad4598c4411a72dd668d9d8d7b2b50f352
Nitesh Kumar <30464868+crazy3553@users.noreply.github.com>
2020-03-21 10:54:09 +0530

    Update store.js

--- src/_helpers/store.js
+++ src/_helpers/store.js
@@ -3,9 +3,7 @@ import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
 import rootReducer from '../_reducers/index';
 
-const loggerMiddleware = createLogger();
-
 export const store = createStore(
   rootReducer,
-  applyMiddleware(thunkMiddleware,loggerMiddleware)
+  applyMiddleware(thunkMiddleware)
 );
